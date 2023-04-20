5a9008a8d48838f704edeef87915bc1c9a3484fe
Hung Tran <nguyentieuhau@gmail.com>
2023-04-20 20:27:07 +0700

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import { inject,  ref, onMounted } from 'vue';
+
+const $cookies = inject('$cookies');
+
+const old_cookie = ref()
+const new_cookie = ref()
+
+
+
+old_cookie.value = $cookies.get('cookie-name');
+
+onMounted(() => {
+    axios
+        .get('/get-cookie')
+        .then(response => {
+            new_cookie.value = $cookies.get('cookie-name');
+        });
+});
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div>
+        <p>Old cookie: {{ old_cookie }}</p>
+        <p>New cookie: {{ new_cookie }}</p>
+    </div>
+</template>
